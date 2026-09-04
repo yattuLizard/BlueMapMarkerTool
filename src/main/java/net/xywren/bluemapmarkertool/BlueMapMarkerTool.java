59f176a391f2bf7fd97e46d73bdec1f4ae971998
yattuLizard <63257664+yattuLizard@users.noreply.github.com>
2026-09-04 16:04:55 +0900

    Bump runtime version to 1.3.3

--- src/main/java/net/xywren/bluemapmarkertool/BlueMapMarkerTool.java
+++ src/main/java/net/xywren/bluemapmarkertool/BlueMapMarkerTool.java
@@ -14,7 +14,7 @@ import org.slf4j.LoggerFactory;
 public class BlueMapMarkerTool {
 
     public static final String MOD_ID = "bluemap_marker_tool";
-    public static final String VERSION = "1.3.2";
+    public static final String VERSION = "1.3.3";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     private MarkerHttpServer httpServer;
